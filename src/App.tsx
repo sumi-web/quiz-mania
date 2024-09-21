19a0463a8f6f74f063bcd2f640e2d16c6327130c
sumi-web <sumity159@gmail.com>
2024-09-21 12:05:56 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+	return (
+		<>
+			<ChakraProvider></ChakraProvider>
+		</>
+	);
+}
+
+export default App;
